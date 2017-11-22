10ee22f6aef591d35faee89065c54abd5a4b8358
Rodrigo Reis <boureal@yahoo.com.br>
2017-11-22 11:43:24 -0200

    Criação de DM e de Projeto

---
+++ Pilotos/King_01/U_DM_01_Server.dfm
@@ -0,0 +1,5 @@
+object Dm_01_Server: TDm_01_Server
+  OldCreateOrder = False
+  Height = 150
+  Width = 215
+end
---
+++ Pilotos/King_01/U_DM_01_Server.pas
@@ -0,0 +1,25 @@
+unit U_DM_01_Server;
+
+interface
+
+uses
+  System.SysUtils, System.Classes;
+
+type
+  TDm_01_Server = class(TDataModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Dm_01_Server: TDm_01_Server;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
